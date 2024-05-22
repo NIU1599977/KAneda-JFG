ae926f25faabf1e35b22c3e6738f9bdcd3a3c673
Göran <1600920@uab.cat>
2024-05-22 14:02:39 +0200

    Volante inercia move , test DC

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ from time import sleep
 from mpu6050 import mpu6050
 import numpy as np 
 from src.kalman import Kalman 
-
+from src.moto import Moto
+import math
 def main():
     my_mpu = mpu6050(0x68) # Initialize MPU to get acceleration and rotation data
     My_Mpu = Kalman(my_mpu) # Object for getting theta and theta_dot
@@ -16,15 +17,15 @@ def main():
     sleep(0.5)
 
     print("Starting the system..!!!")
+    motoClass = Moto()
+    
     while 1:
         t_now = datetime.now()
         dt = (t_now - t_init).total_seconds()*1000 #Time in millis
         if dt> 0:
             aa, bb = My_Mpu.get_angle(units = 'deg')
             print("Theta [deg] = ", int(aa)," Theta_dot [deg/s] = ", int(bb), " loop time[ms] = ", np.round(dt, 2))
-            # Si esto va, hay que mirar como mover el volante de inercia para contrarrestarlo y mantenerse en pie
-            # Si aa es > o < 0 se está inclinando a un lado, por lo que hay que girar el volante hacia el otro lado
-            # Tb dependiendo de la velocidad angular sabremos si se está inclinando demasiado rápido por lo que dependiendo de este valor, le daremos más caña al volante o menos caña
+            motoClass.move_volanteInercia(aa, math.radians(bb))  
         t_init = t_now
 
 if __name__ == '__main__':
