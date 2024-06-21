3b8069fc7753e0cb0e4f644eb3e72a7361d5d010
valdivieso <valdivieso@ieec.cat>
2024-06-21 02:10:52 +0200

    parece que da buenos resultados. pero el dt=0.

--- code/tests/kalman_test.py
+++ code/tests/kalman_test.py
@@ -5,14 +5,14 @@ from time import sleep
 from mpu6050 import mpu6050
 import RPi.GPIO as GPIO
 
-# from libraries.kalman_filterpy import Kalman 
-from libraries.kalman import Kalman
+from libraries.kalman_filterpy import Kalman 
+# from libraries.kalman import Kalman
 from src.moto import Moto
 
 
 def kalman(motoClass):
     my_mpu = mpu6050(0x68) # Initialize MPU to get acceleration and rotation data
-    My_Mpu = Kalman(my_mpu, False) # Object for getting theta and theta_dot
+    My_Mpu = Kalman(my_mpu) # Object for getting theta and theta_dot
     dt = 0.01
 
     sleep(0.1)
@@ -23,17 +23,17 @@ def kalman(motoClass):
         while True:
             t_now = datetime.now()
             # Obtener datos del IMU
-            # z = My_Mpu.get_imu_data(my_mpu, dt)
-            # kf = My_Mpu.kf
+            z = My_Mpu.get_imu_data(my_mpu, dt)
+            kf = My_Mpu.kf
             # Predicción del estado
-            # kf.predict()
+            kf.predict()
             
             # Actualización del estado con la nueva observación
-            # kf.update(z)
+            kf.update(z)
             
             # Obtén el estado estimado
-            # angle_estimated, angular_velocity_estimated = kf.x
-            angle_estimated, angular_velocity_estimated = My_Mpu.get_angle('deg')
+            angle_estimated, angular_velocity_estimated = kf.x
+            # angle_estimated, angular_velocity_estimated = My_Mpu.get_angle('deg')
 
             # Imprime o utiliza el estado estimado
             print(f"Angle: {angle_estimated}, Angular Velocity: {angular_velocity_estimated}")
