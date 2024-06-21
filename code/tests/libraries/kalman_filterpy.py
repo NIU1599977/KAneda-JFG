8d9cf5df07bc6cb834725c0dc3c3d30c473e8ba2
valdivieso <valdivieso@ieec.cat>
2024-06-21 02:09:34 +0200

    filterpy kalman implementado

--- code/tests/libraries/kalman_filterpy.py
+++ code/tests/libraries/kalman_filterpy.py
@@ -6,61 +6,67 @@ from datetime import datetime
 
 class Kalman:
     
-    def __init__(self, mpu):
+    def __init__(self, mpu, calibrate=True):
         self.error = [0.3, -0.75, -2.45]
-        gyro_var, accel_var = self.calibration_imu(mpu)
+        if calibrate:
+            calib = []
+            print("Starting MPU calibration..."); sleep(1)
+            gyro_data = []
+            accel_data = []
+            for i in range(100):
+
+                xx = [mpu.get_accel_data()['y'], mpu.get_accel_data()['z'] - 9.8,
+                        mpu.get_gyro_data()['x']]
+                
+                calib.append(xx)
+
+            calib = np.array(calib) # Make is a 100*3 matrix
+            
+            self.error = np.mean(calib, axis = 0)                
+        
+        print("MPU  calibrated, corrections Y, Z, Omega_x = ", round(self.error[0], 2), round(self.error[1], 2), round(self.error[2], 2))             
+
+        print('Calculating initial conditions for Kalman filter')
+        R = [] # For initial values and sensor covariance matrix
+        
+        for i in range(100):
+            
+            f1 = np.arctan((mpu.get_accel_data()['y'] - self.error[0]) / (mpu.get_accel_data()['z'] - self.error[1]))
+            f2 = np.deg2rad(mpu.get_gyro_data()['x'] - self.error[2]) # Gyro bias [rad] 
+            
+            R.append([f1,f2]) # 100 samples of angle and angular velocity (bias in gyro) in [rad] and [rad/s] respectively
+            
+        R = np.array(R); # Make it an array of 100*2
+       
+        init_conditions = np.mean(R, axis = 0).reshape(2,1) # Get initial conditions in 2*1 form
+       
+        print('Calculated initial states for Kalman filter Acc angle, Gyro bias (constant inaccuracy) ', init_conditions)   
+        
+
         self.kf = KalmanFilter(dim_x=2, dim_z=2)
         
         # Matriz de transición del estado
-        dt = 0.01  # Intervalo de tiempo (ejemplo: 10 ms)
-        self.kf.F = np.array([[1, dt],
+        dt = 0.03  # Intervalo de tiempo (ejemplo: 10 ms)
+        self.kf.F = np.array([[1, -dt],
                         [0, 1]])
         
         # Matriz de observación
-        self.kf.H = np.array([[1, 0],
-                        [0, 1]])
+        self.kf.H = np.eye(2) # matriz identidad
         
         # Matriz de covarianza del proceso
-        q_var = 0.006  # Varianza del proceso
-        self.kf.Q = Q_discrete_white_noise(dim=2, dt=dt, var=q_var)
+        q_var = 0.006  # Varianza del proceso, si vemos que driftea mucho es porque tenemos que aumentarlo, si vemos que va muy lento es porque tenemos que bajarlo
+        self.kf.Q = Q_discrete_white_noise(dim=2, dt=dt, var=q_var) # process noise covarianze matrix
+        # self.Q = np.diag([0.01, 0.03]) # si va lento, hay que bajar 0.01. si driftea mucho hay que aumentar 0.03
         
         # Matriz de covarianza de la observación
-        r_var = 0.03  # Varianza de la observación
-        self.kf.R = np.array([[r_var, 0],
-                        [0, r_var]])
+        self.kf.R = 0.03  # si es muy alto, el filtro responderá muy lento ya que no tendrá en cuenta las medidas, si es muy bajo, se nos colará ruido del acelerometro
+        # self.kf.R = np.array([[r_var, 0], [0, r_var]])
         
         # Inicialización del estado
-        self.kf.x = np.array([[0], [0]])
+        self.kf.x = init_conditions 
         
         # Matriz de covarianza del estado inicial
-        self.kf.P = np.eye(2)
-    
-    def calibration_imu(self, my_mpu):
-        calib = []
-
-        print("Starting MPU calibration..."); sleep(1)
-        gyro_data = []
-        accel_data = []
-        for i in range(100):
-
-            xx = [my_mpu.get_accel_data()['y'], my_mpu.get_accel_data()['z'] - 9.8,
-                    my_mpu.get_gyro_data()['x']]
-            
-            gyro_data.append(my_mpu.get_gyro_data()['x'])
-            accel_data.append(my_mpu.get_accel_data()['y'])  # Usamos 'y' como ejemplo
-            
-            calib.append(xx)
-
-        gyro_var = np.var(gyro_data)
-        accel_var = np.var(accel_data)
-            
-        calib = np.array(calib) # Make is a 100*3 matrix
-        
-        self.error = np.mean(calib, axis = 0)
-        
-        print("MPU  calibrated, corrections Y, Z, Omega_x = ", round(self.error[0], 2), round(self.error[1], 2), round(self.error[2], 2))
-        
-        return gyro_var, accel_var
+        self.kf.P = np.eye(2) # matriz identidad
 
 
     def get_imu_data(self, my_mpu, dt):
@@ -69,8 +75,8 @@ class Kalman:
         
         # Supongamos que el ángulo se obtiene integrando la velocidad angular
         # (esto es una simplificación; normalmente necesitarías un filtro complementario o algo similar)
-        acc_angle = np.arctan2(accel_data['y'] - self.error[0], accel_data['z'] - self.error[1])
-        angular_velocity = gyro_data['x'] - self.error[2]
+        acc_angle = np.rad2deg(np.arctan2(accel_data['y'] - self.error[0], accel_data['z'] - self.error[1])) # arctan devuelve en radianes
+        angular_velocity = gyro_data['x'] - self.error[2] # º/s
         angle = angular_velocity * dt * 0.996 + acc_angle * (1.0 - 0.996)
 
         
